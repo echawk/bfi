aca5a8d15f78175a481a6f7a44827d079a5663e4
Ethan <ethan.hawk@valpo.edu>
2022-05-20 13:17:01 -0500

    bfi.c: cleanup & fix warnings

--- bfi.c
+++ bfi.c
@@ -9,8 +9,6 @@ A totally unoriginal brainfuck interpreter written in C.
 
 void print_tape();
 char *read_file(char *file_path);
-void print_char_arr(char *arr, int len);
-void print_debug();
 
 #define TAPE_SIZE 10
 int tape[TAPE_SIZE];
@@ -41,6 +39,7 @@ int main(int argc, char*argv[]) {
   input_arr = read_file(infile);
   input_pos = 0;
   while (input_arr[input_pos] != 0) {
+    next_char:
     ch = (char)input_arr[input_pos];
     switch (ch) {
     case '+':
@@ -75,7 +74,6 @@ int main(int argc, char*argv[]) {
       break;
     }
     input_pos++;
-    next_char:
   }
   printf("\n");
 
@@ -98,30 +96,9 @@ char *read_file(char *file_path) {
 
   return buffer;
 }
-
-void print_debug() {
-  printf("----------\n");
-  printf("loop depth: %d\n", loop_depth);
-  printf("loop_depth_indices[%d]: %d\n", loop_depth, loop_depth_indices[loop_depth]);
-  printf("current char: %c\n", ch);
-  printf("input_arr[%d]: %c\n", input_pos, input_arr[input_pos]);
-  printf("input_pos: %d\n", input_pos);
-  printf("tape position: %d\n", pos);
-  print_tape();
-  printf("----------\n");
-}
-
 void print_tape() {
-  int i = 0;
-  while (i < TAPE_SIZE) {
-    printf("tape[%d]: %d\n", i, tape[i]);
-    i++;
-  }
-}
-
-void print_char_arr(char *arr, int len) {
   int i;
-  for (i = 0; i < len; i++) {
-    printf("arr[%d] = %c\n", i, arr[i]);
+  for (i = 0; i < TAPE_SIZE; i++) {
+    printf("tape[%d]: %d\n", i, tape[i]);
   }
 }
